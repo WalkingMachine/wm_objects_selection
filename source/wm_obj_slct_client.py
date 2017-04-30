2913a95608ee3ac481e9cdbb4e03328738da7bc2
f3nr1r <sotistechno@gmail.com>
2017-04-30 17:07:11 -0400

    Find requested object in db or tells otherwise if not detected

--- source/wm_obj_slct_client.py
+++ source/wm_obj_slct_client.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 import rospy
+import os
 import sys
-from object_recognition_msgs.msg import *
+import object_recognition_msgs
+import object_recognition_core
+import object_recognition_ros
 import std_msgs.msg
+from object_recognition_msgs.msg import *
 from wm_object_selection.srv import *
+from object_recognition_ros import *
+
 
 
 class select_object_client():
@@ -15,13 +21,13 @@ class select_object_client():
 
     def client(self,array,filter):
         rospy.wait_for_service('slct_obj')
-        print "Fetching object for you"
         try:
             self.inputs_srv = rospy.ServiceProxy('slct_obj', rcgnzd_obj)
             self.slct_obj = self.inputs_srv(array,filter)
 
         except rospy.ServiceException, e:
             print "Service call failed: %s" % e
+
 
 
 if __name__ == "__main__":
--- source/wm_obj_slct_srv.py
+++ source/wm_obj_slct_srv.py
@@ -1,26 +1,58 @@
 #!/usr/bin/env python
+# coding=utf-8
 import rospy
+import os
+import object_recognition_core
 import geometry_msgs.msg
 import sensor_msgs.msg
 import shape_msgs.msg
 from object_recognition_msgs.msg import *
+from object_recognition_msgs.srv import *
 from object_recognition_core import *
 import std_msgs.msg
 from wm_object_selection.srv import *
 
-sub_once = None
-
 
 def handle_slt_obj(req):
-    print "This is ",req.filter, "'s position :",req.objectarray.objects[0].pose
+    objects_info_list = []
+    position = 0
+
+    print "Request for ", req.filter, "registered"
+    # Construit la liste des info d'objets détectés
+    for i in req.objectarray.objects:
+        objects_info_list.append(client_info(i.type))
+
+    # Parcours la liste d'informations pour trouver l'objet demandé
+    for i in objects_info_list:
+        if i.name == req.filter:
+            print "Object found: The object with id",req.objectarray.objects[position].type.key, "is :",i.name
+            break
+        else:
+            rospy.logout("Object not found, try again")
+        position = position +1
+
+
+    rospy.logout("Service select_object_server waiting")
     return  req.objectarray.objects[0]
 
 
 def slct_obj_srv():
     rospy.init_node('select_object_server')
     s = rospy.Service('slct_obj', rcgnzd_obj , handle_slt_obj)
-    rospy.logout("Service select_object_server called")
+    rospy.logout("Service select_object_server waiting")
+    os.system("rosrun object_recognition_ros object_information_server")
     rospy.spin()
+
+def client_info(type):
+    rospy.wait_for_service('get_object_info')
+    print "Retrieving object info"
+    try:
+        h_obj_info = rospy.ServiceProxy('get_object_info', GetObjectInformation)
+        info = h_obj_info(type)
+        return info.information
+
+    except rospy.ServiceException, e:
+        print "Service get_object_info failed: %s" % e
 
 
 if __name__ == "__main__":
