057e712ee183764a89b79cd0053f9691992c926f
Samuel Otis <sotistechno@gmail.com>
2017-06-05 20:47:01 -0400

    Basic image manip on scene of detected objects

--- source/wm_obj_slct_srv.py
+++ source/wm_obj_slct_srv.py
@@ -2,13 +2,15 @@
 # coding=utf-8
 import rospy
 import os
+import cv2
 import object_recognition_core
 import geometry_msgs.msg
 import shape_msgs.msg
+import std_msgs.msg
 from object_recognition_msgs.msg import *
 from object_recognition_msgs.srv import *
 from object_recognition_core import *
-import std_msgs.msg
+from cv_bridge import CvBridge, CvBridgeError
 from wm_object_selection.srv import *
 from sensor_msgs.msg import PointCloud2
 
@@ -21,11 +23,18 @@ def handle_slt_obj(req):
     # Construit la liste des info d'objets détectés
     for i in req.objectarray.objects:
         objects_info_list.append(client_info(i.type))
+        bounding_box(req.image, i.pose.pose.pose.position)
+
 
     # Parcours la liste d'informations pour trouver l'objet demandé
     for i in objects_info_list:
         if i.name == req.filter:
-            rospy.logdebug('Object found: The object with id'+ str(req.objectarray.objects[position].type.key)+ 'is :'+ str(i.name)+ '\n\rat position\r\n'+ str(req.objectarray.objects[position].pose.pose.pose.position))
+            rospy.logdebug('Object found: The object with id'
+                           + str(req.objectarray.objects[position].type.key)
+                           + 'is :'
+                           + str(i.name)
+                           + '\n\rat position\r\n'
+                           + str(req.objectarray.objects[position].pose.pose.pose.position))
             rospy.logout("Service select_object_server waiting")
             return req.objectarray.objects[position].pose
         else:
@@ -51,6 +60,17 @@ def client_info(type):
 
     except rospy.ServiceException, e:
         rospy.logerr("Service get_object_info failed: %s" +str(e))
+
+def bounding_box(image, position):
+    bridge = CvBridge()
+    try:
+        cv_image = bridge.imgmsg_to_cv2(image, "passthrough")
+    except CvBridgeError as e:
+        print(e)
+    print position
+    cv2.rectangle(cv_image, (0, 0), (20, 20), (255,0,0), 2)
+    cv2.imshow("Segmented objects", cv_image)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
