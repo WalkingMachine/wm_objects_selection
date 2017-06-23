71105d367757433f708d41683e6e795677c40e39
Samuel Otis <sotistechno@gmail.com>
2017-06-23 16:20:08 -0400

    Retrieve bounding box - Corrected issue #2

--- script/wm_obj_slct_server.py
+++ script/wm_obj_slct_server.py
@@ -20,11 +20,14 @@ def handle_slt_obj(req):
     position = 0
     rospy.logdebug("Request for ", req.filter, "registered")
     rospy.logout('Retrieving object information')
+
     # Construit la liste des info d'objets détectés
     for i in req.objectarray.objects:
         objects_info_list.append(client_info(i.type))
-        bounding_box(req.image, i.pose.pose.pose.position)
-
+        bounding_box(req.image,
+                     i.pose.pose.pose.position,
+                     objects_info_list[-1].name,
+                     objects_info_list[-1].ground_truth_mesh)
 
     # Parcours la liste d'informations pour trouver l'objet demandé
     for i in objects_info_list:
@@ -51,6 +54,7 @@ def slct_obj_srv():
     os.system("rosrun object_recognition_ros object_information_server")
     rospy.spin()
 
+
 def client_info(type):
     rospy.wait_for_service('get_object_info')
     try:
@@ -59,18 +63,71 @@ def client_info(type):
         return info.information
 
     except rospy.ServiceException, e:
-        rospy.logerr("Service get_object_info failed: %s" +str(e))
+        rospy.logerr("Service get_object_info failed: %s" + str(e))
 
-def bounding_box(image, position):
+
+def bounding_box(image, position, name, mesh):
     bridge = CvBridge()
+    x_val = []
+    z_val = []
     try:
         cv_image = bridge.imgmsg_to_cv2(image, "passthrough")
     except CvBridgeError as e:
         print(e)
-    print position
-    cv2.rectangle(cv_image, (0, 0), (20, 20), (255,0,0), 2)
-    cv2.imshow("Segmented objects", cv_image)
-    cv2.waitKey(0)
+    print name
+
+    f = 0.00966
+    fx = 525.0  # default focal length x
+    fy = 525.0  # default focal length y
+    cx = 319.5  # default optical center x
+    cy = 239.5  # default optical center y
+
+    # Extract 3d position of object
+    z_r = position.z
+    y_r = position.y
+    x_r = position.x
+
+    # Transfer to pixel frame
+    d = z_r / f
+    u = x_r * fx / z_r + cx
+    v = y_r * fy / z_r + cy
+    x_p = int(u) - 20
+    y_p = int(v)
+
+    # Extract width and length of object in millimeters
+    for i in mesh.vertices:
+        x_val.append(i.x)
+        z_val.append(i.z)
+    x_max = max(x_val) * 1000
+    z_max = max(z_val) * 1000
+
+    # Transfer object size to pixel frame
+    size_v = int(x_max / z_r * 2)
+    size_u = int(z_max / z_r * 2)
+
+    # Draw bounding box
+    cv2.rectangle(cv_image, (x_p-size_u/2, y_p-size_v/2), (x_p+size_u/2, y_p+size_v/2), (255, 0, 0), 2)
+    cv2.putText(cv_image, name, (x_p-size_u/2, y_p-size_v/2), 0, 1, 0xFF, 2)
+
+    # Crops individual object
+    crop_img = cv_image[y_p - size_v / 2 - 20:y_p + size_v / 2, x_p - size_u / 2:x_p + size_u / 2]
+
+    # Resize object
+    r = 640.0 / crop_img.shape[1]
+    dim = (640, int(crop_img.shape[0] * r))
+
+    # perform the actual resizing of the image and show it
+    crop_img_resized = cv2.resize(crop_img, dim, interpolation=cv2.INTER_AREA)
+
+    # Saves different images to directory
+    path = os.path.expanduser('~/roi_images_objects')
+    if(os.path.exists(path)):
+        os.system('rm -r ' + path)
+    os.system('mkdir roi_images_objects')
+    os.chdir(os.path.expanduser('~/roi_images_objects/'))
+    cv2.imwrite(name + '.png', crop_img_resized)
+    cv2.imwrite('scene.png', cv_image)
+    os.chdir(os.path.expanduser('~/'))
 
 
 if __name__ == "__main__":
